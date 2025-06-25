d905896c9186901d502fe91ee8ca6da6ce9c7c06
ndk <navnathkadam284@gmail.com>
2025-06-25 21:32:00 +0530

    Namespace concept

---
+++ Namespaces/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MyMathUtils.Calculator;
+
+namespace MainNameSpace
+{
+    internal class MyCalculator
+    {
+        public static void Main(string[] args)
+        {
+            int a = 10, b = 20;
+            Console.WriteLine("Add: {0}", Add.Addition(a, b));
+
+            Console.WriteLine("Sub: {0}", Sub.Substraction(a, b));
+
+            Console.WriteLine("Mul: {0}", Mul.Multiplication(a, b));
+            
+            Console.WriteLine("Div: {0}", Div.Division(a, b));
+
+        }
+    }
+}
---
+++ Namespaces/Utils/MyUtils.cs
@@ -0,0 +1,35 @@
+
+namespace MyMathUtils.Calculator
+{
+    public class Add
+    {
+        public static int Addition(int a, int b)
+        {
+            return (a + b);
+        }
+    }
+
+    public static class Sub
+    {
+        public static int Substraction(int a, int b)
+        {
+            return (a - b);
+        }
+    }
+
+    public static class Mul
+    {
+        public static int Multiplication(int a, int b)
+        {
+            return (a * b);
+        }
+    }
+
+    public static class Div
+    {
+        public static int Division(int a, int b)
+        {
+            return (a / b);
+        }
+    }
+}
